d1b7211426bc7215f713a4e946781325078318c7
robbie <robbiekruszynski@gmail.com>
2021-04-01 14:13:13 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { UPDATE_THIS } from "./abi/abi";
+import { ethers } from 'ethers';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+         TIME TO BUIDL
+        </h1>
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
